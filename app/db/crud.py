d03d069b9f177727667163840fbcfe88b0839055
Игорь <idemenko22@gmail.com>
2026-03-16 22:34:02 +1000

    feat(db) add crud for User

---
+++ app/db/crud.py
@@ -0,0 +1,46 @@
+from sys import winver
+
+from sqlalchemy import select, update
+from app.db.models import User, BankAccount, BankOperation
+from app.db.database import async_session
+
+async def create_user(telegram_id: int):
+    async with async_session() as session:
+        user = User(
+            telegram_id=telegram_id
+        )
+        session.add(user)
+
+        await session.commit()
+        await session.refresh(user)
+        return user
+
+async def get_user(id: int):
+    async with async_session() as session:
+        user = await session.execute(
+            select(User).where(User.id == id)
+        )
+
+        return user.scalar_one_or_none()
+
+async def get_user_all():
+    async with async_session() as session:
+        users = await session.execute(
+            select(User)
+        )
+        return users.scalars().all()
+
+async def update_user_is_active(id: int, is_active: bool):
+    async with async_session() as session:
+        result = session.execute(
+            select(User).where(User.id == id)
+        )
+        user = result.scalar_one_or_none()
+
+        if not user:
+            return None
+
+        user.is_active = is_active
+        await session.commit()
+        await session.refresh(user)
+        return user
