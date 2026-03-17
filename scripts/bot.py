845053fd89aaa8d411fac26a72ebaee517c6e9fc
Игорь <idemenko22@gmail.com>
2026-03-17 22:52:13 +1000

    feat(scripts): add init db in bot start

--- scripts/bot.py
+++ scripts/bot.py
@@ -4,9 +4,13 @@ from app.bot.bot import bot, dp
 from app.bot.handler.user import user_router_bot
 from app.config.logging_config import setup_logging
 
+from app.db.init_db import init_models
+
 setup_logging()
 
 async def start_bot():
+    await init_models()
+
     dp.include_router(user_router_bot)
     await dp.start_polling(bot)
 
