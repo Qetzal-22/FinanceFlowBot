1585c0a46745ac6c3e93e009d597308c971c438f
Игорь <idemenko22@gmail.com>
2026-03-19 02:50:30 +1000

    feat(db): add crud for category and user_categories

--- app/db/crud.py
+++ app/db/crud.py
@@ -1,8 +1,12 @@
-from sqlalchemy import select
-from app.db.models import User, BankAccount, BankOperation, Type_Operation
+from sqlalchemy import select, delete
+from unicodedata import category
+
+from app.bot.handler.user import user_router_bot
+from app.db.models import User, BankAccount, BankOperation, Type_Operation, Category, UserCategory
 from app.db.database import async_session
 
 import asyncio
+
 
 async def create_user(telegram_id: int):
     async with async_session() as session:
@@ -15,6 +19,7 @@ async def create_user(telegram_id: int):
         await session.refresh(user)
         return user
 
+
 async def get_user(id: int):
     async with async_session() as session:
         user = await session.execute(
@@ -22,6 +27,7 @@ async def get_user(id: int):
         )
 
         return user.scalar_one_or_none()
+
 
 async def get_user_by_telegram_id(telegram_id: int):
     async with async_session() as session:
@@ -31,12 +37,14 @@ async def get_user_by_telegram_id(telegram_id: int):
 
         return user.scalar_one_or_none()
 
+
 async def get_user_all():
     async with async_session() as session:
         users = await session.execute(
             select(User)
         )
         return users.scalars().all()
+
 
 async def update_user_is_active(id: int, is_active: bool):
     async with async_session() as session:
@@ -53,6 +61,7 @@ async def update_user_is_active(id: int, is_active: bool):
         await session.refresh(user)
         return user
 
+
 async def create_bank_account(user_id: int, name: str):
     async with async_session() as session:
         account = BankAccount(user_id=user_id, name=name)
@@ -62,6 +71,7 @@ async def create_bank_account(user_id: int, name: str):
         await session.refresh(account)
         return account
 
+
 async def get_bank_account(id: int):
     async with async_session() as session:
         account = await session.execute(
@@ -70,6 +80,7 @@ async def get_bank_account(id: int):
 
         return account.scalar_one_or_none()
 
+
 async def get_bank_account_all():
     async with async_session() as session:
         accounts = await session.execute(
@@ -77,16 +88,17 @@ async def get_bank_account_all():
         )
         return accounts.scalars().all()
 
+
 async def get_bank_accounts_by_telegram_id(telegram_user_id: int):
     async with async_session() as session:
         accounts = await session.execute(
-            select(BankAccount).join(User).where(User.telegram_id==telegram_user_id)
+            select(BankAccount).join(User).where(User.telegram_id == telegram_user_id)
         )
 
     return accounts.scalars().all()
 
 
-async def update_bank_account(id:int, name: str = None, balance: float = None):
+async def update_bank_account(id: int, name: str = None, balance: float = None):
     async with async_session() as session:
         result = await session.execute(
             select(BankAccount).where(BankAccount.id == id)
@@ -105,21 +117,24 @@ async def update_bank_account(id:int, name: str = None, balance: float = None):
         await session.refresh(account)
         return account
 
+
 async def create_bank_operation(
-                                account_id: int,
-                                type: Type_Operation,
-                                amount: float,
-                                balance_after: float,
-                                description: str,
-                                category: str
-                                ):
+        account_id: int,
+        type: Type_Operation,
+        amount: float,
+        balance_after: float,
+        description: str,
+        category: str
+):
     async with async_session() as session:
-        operation = BankOperation(account_id=account_id, type=type, amount=amount, description=description, category=category)
+        operation = BankOperation(account_id=account_id, type=type, amount=amount, description=description,
+                                  category=category)
 
         session.add(operation)
         await session.commit()
         await session.refresh(operation)
         return operation
+
 
 async def get_operation(id: int):
     async with async_session() as session:
@@ -129,6 +144,7 @@ async def get_operation(id: int):
 
         return operation.scalar_one_or_none()
 
+
 async def get_operation_all():
     async with async_session() as session:
         operations = await session.execute(
@@ -137,7 +153,9 @@ async def get_operation_all():
 
         return operations.scalars().all()
 
-async def update_operation(id: int, type: Type_Operation = None, amount: float = None, balance_after: float = None, description: str = None, category: str = None):
+
+async def update_operation(id: int, type: Type_Operation = None, amount: float = None, balance_after: float = None,
+                           description: str = None, category: str = None):
     async with async_session() as session:
         result = await session.execute(
             select(BankOperation).where(BankOperation.id == id)
@@ -165,3 +183,87 @@ async def update_operation(id: int, type: Type_Operation = None, amount: float =
         await session.commit()
         await session.refresh(operation)
         return operation
+
+
+async def create_category(name: str):
+    async with async_session() as session:
+        category = Category(name=name)
+        session.add(category)
+        await session.commit()
+        await session.refresh(category)
+        return category
+
+
+async def get_category(id: int):
+    async with async_session() as session:
+        category = await session.execute(
+            select(Category).where(Category.id == id)
+        )
+
+        return category.scalar_one_or_none()
+
+
+async def get_categories_all():
+    async with async_session() as session:
+        categories = await session.execute(
+            select(Category)
+        )
+
+        return categories.scalars().all()
+
+
+async def get_categories_all_by_user_id(user_id: int):
+    async with async_session() as session:
+        categories = await session.execute(
+            select(Category).join(UserCategory).where(UserCategory.user_id == user_id)
+        )
+
+        return categories.scalars().all()
+
+
+async def create_user_category(user_id, category_id):
+    async with async_session() as session:
+        user_category = UserCategory(user_id=user_id, category_id=category_id)
+        session.add(user_category)
+        await session.commit()
+        await session.refresh(user_category)
+        return user_category
+
+
+async def get_user_category(id: int):
+    async with async_session() as session:
+        user_category = await session.execute(
+            select(UserCategory).where(UserCategory.id == id)
+        )
+
+        return user_category.scalar_one_or_none()
+
+
+async def get_user_categories_by_user_id(user_id: int):
+    async with async_session() as session:
+        user_categories = await session.execute(
+            select(UserCategory).where(UserCategory.user_id == user_id)
+        )
+
+        return user_categories.scalars().all()
+
+
+async def get_user_categories_by_telegram_user_id(telegram_user_id: int):
+    async with async_session() as session:
+        user_categories = await session.execute(
+            select(UserCategory).join(User).where(User.telegram_user_id == telegram_user_id)
+        )
+
+        return user_categories.scalars().all()
+
+
+async def delete_user_category(user_id: int, category_id: int):
+    async with async_session() as session:
+        await session.execute(
+            delete(UserCategory).where(
+                UserCategory.user_id == user_id,
+                UserCategory.category_id == category_id
+            )
+        )
+
+        await session.commit()
