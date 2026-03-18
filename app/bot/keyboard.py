1a429a4ca2404424cce2733381397a1f3ef3ef21
Игорь <idemenko22@gmail.com>
2026-03-19 02:48:17 +1000

    feat(bot): add account keyboard

--- app/bot/keyboard.py
+++ app/bot/keyboard.py
@@ -47,3 +47,31 @@ async def choose_type_transaction_kb():
     kb.button(text="Expense", callback_data="type_transaction:expense")
     return kb.as_markup()
 
+async def main_bank_account_kb():
+    kb = ReplyKeyboardBuilder()
+    kb.button(text="Create new account")
+    kb.button(text="Edit account")
+    kb.button(text="Back to menu")
+    kb.adjust(2, 1)
+    return kb.as_markup(resize_keyboard=True)
+
+async def choose_account_kb(accounts: list[BankAccount]):
+    kb = InlineKeyboardBuilder()
+    for account in accounts:
+        kb.button(text=f"{account.name}", callback_data=f"account:{account.id}")
+    return kb.as_markup()
+
+async def choose_account_for_transaction_kb(accounts: list[BankAccount]):
+    kb = InlineKeyboardBuilder()
+    for account in accounts:
+        kb.button(text=f"{account.name}", callback_data=f"transaction_account:{account.id}")
+    return kb.as_markup()
+
+async def edit_account_kb():
+    kb = ReplyKeyboardBuilder()
+    kb.button(text="Record transaction")
+    kb.button(text="Rename")
+    kb.button(text="Remove")
+    kb.button(text="Back to menu")
+    kb.adjust(1, 2, 1)
+    return kb.as_markup(resize_keyboard=True)
