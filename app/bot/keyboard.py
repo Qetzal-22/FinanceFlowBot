72e398d7a60b4210062211b93711c05c3d3d7eeb
Игорь <idemenko22@gmail.com>
2026-03-19 02:45:19 +1000

    feat(bot): add main_menu and profile keyboard

--- app/bot/keyboard.py
+++ app/bot/keyboard.py
@@ -1,4 +1,7 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+
+from app.db.models import BankAccount, Category
+
 
 async def register_kb():
     kb = InlineKeyboardBuilder()
@@ -9,3 +12,18 @@ async def create_bank_account_kb():
     kb = InlineKeyboardBuilder()
     kb.button(text="Create Account", callback_data="create_account")
     return kb.as_markup()
+
+async def main_menu_kb():
+    kb = ReplyKeyboardBuilder()
+    kb.button(text="Record Transaction")
+    kb.button(text="Bank Account")
+    kb.button(text="Profile")
+    kb.adjust(1, 2)
+    return kb.as_markup(resize_keyboard=True)
+
+async def profile_kb():
+    kb = ReplyKeyboardBuilder()
+    kb.button(text="My category")
+    kb.button(text="Back to menu")
+    return kb.as_markup(resize_keyboard=True)
+
