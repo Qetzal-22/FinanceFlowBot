489dec145b9f170a5fae886496c607595e963768
Игорь <idemenko22@gmail.com>
2026-03-19 02:49:50 +1000

    feat(static): add CreateTransaction and AddCategory statics

--- app/bot/static.py
+++ app/bot/static.py
@@ -2,3 +2,13 @@ from aiogram.fsm.state import State, StatesGroup
 
 class CreateBankAccount(StatesGroup):
     name = State()
+
+class CreateTransaction(StatesGroup):
+    account_id = State()
+    type = State()
+    amount = State()
+    description = State()
+    category = State()
+
+class AddCategory(StatesGroup):
+    name = State()
