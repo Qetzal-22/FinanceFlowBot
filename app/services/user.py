fc51bf85686671d05bd349dcc688d696d29e66c2
Игорь <idemenko22@gmail.com>
2026-03-19 02:08:37 +1000

    feat(services): add check register function

--- app/services/user.py
+++ app/services/user.py
@@ -8,3 +8,13 @@ async def create_user(telegram_user_id: int):
     await crud.create_user(telegram_user_id)
     logger.info("DB successful response create user telegram_user_id=%s", telegram_user_id)
     return True
+
+async def check_register(telegram_user_id: int) -> bool:
+    """True - is register | False - isn`t register"""
+    logger.info("DB request get user telegram_user_id=%s", telegram_user_id)
+    user = await crud.get_user_by_telegram_id(telegram_user_id)
+    logger.info("DB successful response get user telegram_user_id=%s", telegram_user_id)
+    if not user:
+        return False
+    return True
+
