9bb53bd53b33e06efcb35146b2f95783600fa326
Игорь <idemenko22@gmail.com>
2026-03-19 02:52:13 +1000

    feat(services): add get bank account

--- app/services/bank_account.py
+++ app/services/bank_account.py
@@ -9,3 +9,10 @@ async def create_account(user_id: int, name: str):
     await crud.create_bank_account(user_id, name)
     logger.info("DB successful response create bank account user_id=%s", user_id)
     return True
+
+async def get_bank_accounts(telegram_user_id: int):
+    logger.info("DB request get bank account telegram_user_id=%s", telegram_user_id)
+    accounts = await crud.get_bank_accounts_by_telegram_id(telegram_user_id)
+    logger.info("DB successful response get bank account telegram_user_id=%s", telegram_user_id)
+
+    return accounts
