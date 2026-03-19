76f15aba02689ff32b6a4022314e9bae4b08b9de
Игорь <idemenko22@gmail.com>
2026-03-19 22:37:55 +1000

    fix(services): fix get_user_categories

--- app/services/user.py
+++ app/services/user.py
@@ -20,8 +20,13 @@ async def check_register(telegram_user_id: int) -> bool:
     return True
 
 async def get_user_categories(telegram_user_id):
+    logger.info("DB request get user telegram_user_id=%s", telegram_user_id)
+    user = await crud.get_user_by_telegram_id(telegram_user_id)
+    logger.info("DB successful response get user telegram_user_id=%s", telegram_user_id)
+    user_id = user.id
+
     logger.info("DB request get categoty telegram_user_id=%s", telegram_user_id)
-    categories = crud.get_user_categories_by_telegram_user_id(telegram_user_id)
+    categories = await crud.get_categories_all_by_user_id(user_id)
     logger.info("DB successful response get categoty telegram_user_id=%s", telegram_user_id)
     return categories
 
@@ -30,6 +35,7 @@ async def create_user_category(telegram_user_id, category_id):
     user = await crud.get_user_by_telegram_id(telegram_user_id)
     logger.info("DB successful response get user telegram_user_id=%s", telegram_user_id)
     user_id = user.id
+
     logger.info("DB request create user_category telegram_user_id=%s category_id=%S", telegram_user_id, category_id)
     await crud.create_user_category(user_id, category_id)
     logger.info("DB successful response create user_category telegram_user_id=%s category_id=%S", telegram_user_id, category_id)
@@ -37,5 +43,5 @@ async def create_user_category(telegram_user_id, category_id):
 async def create_category(name: str):
     logger.info("DB request create category name=%s", name)
     category = await crud.create_category(name)
-    logger.info("DB request create category name=%s", name)
+    logger.info("DB successful response create category name=%s", name)
     return category
