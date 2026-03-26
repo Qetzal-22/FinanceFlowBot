49113fe7541fdb8e9ba396fc16999fc26fe1bdda
Игорь <idemenko22@gmail.com>
2026-03-27 01:41:23 +1000

    feat(services): add update and delete account

--- app/services/bank_account.py
+++ app/services/bank_account.py
@@ -16,3 +16,14 @@ async def get_bank_accounts(telegram_user_id: int):
     logger.info("DB successful response get bank account telegram_user_id=%s", telegram_user_id)
 
     return accounts
+
+async def update_account(account_id: int, name: str = None, balance: float = None):
+    logger.info("DB request update account account_id=%s", account_id)
+    await crud.update_bank_account(account_id, name, balance)
+    logger.info("DB successful response update account account_id=%s", account_id)
+
+
+async def delete_account(account_id: int):
+    logger.info("DB request delete account account_id=%s", account_id)
+    await crud.delete_bank_account(account_id)
+    logger.info("DB successful response delete account account_id=%s", account_id)
