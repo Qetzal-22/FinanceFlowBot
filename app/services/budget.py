30f943e21f8819efcc1639c9e9d1c5d3b81c8378
Игорь <idemenko22@gmail.com>
2026-03-31 17:01:10 +1000

    feat(services): add create budget

--- app/services/budget.py
+++ app/services/budget.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from app.db import crud
 
@@ -9,3 +10,12 @@ async def get_budget_by_user_category_id(user_category_id):
     budget = await crud.get_budget_by_user_category_id(user_category_id)
     logger.info("DB successful response get budget by user_category_id=%s", user_category_id)
     return budget
+
+async def create_budget(user_category_id: int, amount: float):
+    now = datetime.now()
+    year = now.year
+    month = now.month
+
+    logger.info("DB request create budget user_category_id=%s year=%s month=%s", user_category_id, year, month)
+    await crud.create_budget(user_category_id, amount, year, month)
+    logger.info("DB successful response create budget user_category_id=%s year=%s month=%s", user_category_id, year, month)
