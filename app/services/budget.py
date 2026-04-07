b7ba85ba85ba357da6e05103fd918450d81da64e
Игорь <idemenko22@gmail.com>
2026-04-07 20:46:12 +1000

    feat(services): add in get budget sort for date

--- app/services/budget.py
+++ app/services/budget.py
@@ -5,11 +5,17 @@ from app.db import crud
 
 logger = logging.getLogger(__name__)
 
-async def get_budget_by_user_category_id(user_category_id):
+async def get_budget_by_user_category_id_now(user_category_id):
+    now = datetime.now()
+    year = now.year
+    month = now.month
+
     logger.info("DB request get budget by user_category_id=%s", user_category_id)
-    budget = await crud.get_budget_by_user_category_id(user_category_id)
+    budgets = await crud.get_budget_by_user_category_id_now(year, month, user_category_id)
     logger.info("DB successful response get budget by user_category_id=%s", user_category_id)
-    return budget
+    if len(budgets) == 0:
+        return None
+    return budgets[0]
 
 async def create_budget(user_category_id: int, amount: float):
     now = datetime.now()
@@ -17,5 +23,5 @@ async def create_budget(user_category_id: int, amount: float):
     month = now.month
 
     logger.info("DB request create budget user_category_id=%s year=%s month=%s", user_category_id, year, month)
-    await crud.create_budget(user_category_id, amount, year, month)
+    await crud.upsert_budget(user_category_id, amount, year, month)
     logger.info("DB successful response create budget user_category_id=%s year=%s month=%s", user_category_id, year, month)
