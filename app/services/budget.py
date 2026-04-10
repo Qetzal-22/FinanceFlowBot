58f4e00fa5a7bb1efb673f386378b66363b92bbc
Игорь <idemenko22@gmail.com>
2026-04-10 13:30:17 +1000

    feat(services): add update budget for new month

--- app/services/budget.py
+++ app/services/budget.py
@@ -1,9 +1,10 @@
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from app.db import crud
 
 logger = logging.getLogger(__name__)
+
 
 async def get_budget_by_user_category_id_now(user_category_id):
     now = datetime.now()
@@ -11,11 +12,12 @@ async def get_budget_by_user_category_id_now(user_category_id):
     month = now.month
 
     logger.info("DB request get budget by user_category_id=%s", user_category_id)
-    budgets = await crud.get_budget_by_user_category_id_now(year, month, user_category_id)
+    budgets = await crud.get_budget_by_user_category_id_to_date(year, month, user_category_id)
     logger.info("DB successful response get budget by user_category_id=%s", user_category_id)
     if len(budgets) == 0:
         return None
     return budgets[0]
+
 
 async def create_budget(user_category_id: int, amount: float):
     now = datetime.now()
@@ -24,4 +26,29 @@ async def create_budget(user_category_id: int, amount: float):
 
     logger.info("DB request create budget user_category_id=%s year=%s month=%s", user_category_id, year, month)
     await crud.upsert_budget(user_category_id, amount, year, month)
-    logger.info("DB successful response create budget user_category_id=%s year=%s month=%s", user_category_id, year, month)
+    logger.info(
+        "DB successful response create budget user_category_id=%s year=%s month=%s",
+        user_category_id,
+        year,
+        month
+    )
+
+
+async def update_budget_new_month():
+    now = datetime.now()
+    year = now.year
+    month = now.month
+
+    if month == 1:
+        last_month = 12
+        last_year = year - 1
+    else:
+        last_month = month - 1
+        last_year = year
+
+    logger.info("DB request get budgets to date year=%s month=%s", last_year, last_month)
+    budgets_last_month = await crud.get_budget_to_date(last_year, last_month)
+    logger.info("DB successful response get budgets to date year=%s month=%s", last_year, last_month)
+
+    for budget_last_month in budgets_last_month:
+        await create_budget(budget_last_month.user_category_id, budget_last_month.amount)
