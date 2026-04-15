43bb9efdb23a65f2ad75a237a167a0b44c63f328
Игорь <idemenko22@gmail.com>
2026-04-15 17:56:05 +1000

    feat(services): add determining is_default for create_account

--- app/services/bank_account.py
+++ app/services/bank_account.py
@@ -5,8 +5,14 @@ from app.db import crud
 logger = logging.getLogger(__name__)
 
 async def create_account(user_id: int, name: str):
+    bank_account = await crud.get_bank_accounts_by_user_id(user_id)
+    if len(bank_account) == 0:
+        default = True
+    else:
+        default = False
+
     logger.info("DB request create bank account user_id=%s", user_id)
-    await crud.create_bank_account(user_id, name)
+    await crud.create_bank_account(user_id, name, default)
     logger.info("DB successful response create bank account user_id=%s", user_id)
     return True
 
