5166db1aafa0c5c83375976b6dfd1b81bdd96b31
Игорь <idemenko22@gmail.com>
2026-04-15 23:28:34 +1000

    feat(services): change create_operation for new UX and add add_description, delete_operation function

--- app/services/bank_operation.py
+++ app/services/bank_operation.py
@@ -8,28 +8,40 @@ from app.db.models import Type_Operation, BankOperation
 
 logger = logging.getLogger(__name__)
 
-async def create_operation(account_id: int, type_operation: Type_Operation, amount: float, description: str, category_id: int = None):
-    account = await crud.get_bank_account(account_id)
-    bank_account = await crud.get_bank_account(account_id)
-    user = bank_account.user
+async def create_operation(user_id: int, type_operation: Type_Operation, amount: float, category_id: int = None):
+    account = await crud.get_default_bank_account_by_user_id(user_id)
+    user = account.user
+    logger.info("create_operation user_id=%s, category_id=%s", user.id, category_id)
     user_category = await crud.get_user_category_by_user_id_and_category_id(user.id, category_id)
     balance = account.balance
+
     logger.info("create_operation balance=%s amount=%s type_operation=%s", balance, amount, type_operation)
     if type_operation == Type_Operation.INCOME:
         balance_after = balance + amount
+        event = None
     else:
         balance_after = balance - amount
         budget = await get_budget_by_user_category_id_now(user_category.id)
-        await crud.edit_budget_add_spend(budget.id, amount)
-        event = await check_overflow_budget(budget.amount, budget.spend+amount)
-    logger.info("create_operation balance_after=%s", balance_after)
-    await crud.update_bank_account(account_id, balance=balance_after)
-    await crud.create_bank_operation(account_id, type_operation, amount, balance_after, description, category_id)
+        if not budget is None:
+            await crud.edit_budget_add_spend(budget.id, amount)
+            event = await check_overflow_budget(budget.amount, budget.spend+amount)
+        else:
+            event = None
 
-    return event
+    logger.info("create_operation balance_after=%s", balance_after)
+    await crud.update_bank_account(account.id, balance=balance_after)
+    operation = await crud.create_bank_operation(account.id, type_operation, amount, balance_after, category_id)
+
+    return operation, event
 
 async def get_bank_operation_by_date(date: datetime) -> list[BankOperation]:
     logger.info("DB request get bank operations by date date=%s", date)
     bank_operations = await crud.get_operation_by_date(date)
     logger.info("DB successful response get bank operation by date date=%s", date)
     return bank_operations
+
+async def add_description_for_operation(operation_id: int, description: str) -> None:
+    await crud.update_operation(operation_id, description=description)
+
+async def delete_operation(operation_id: int) -> None:
+    await crud.delete_bank_operation(operation_id)
