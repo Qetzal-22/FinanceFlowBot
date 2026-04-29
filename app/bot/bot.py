a11e51b9071c11f801be075698464f5e5fea1b49
Игорь <idemenko22@gmail.com>
2026-04-29 15:01:44 +1000

    chore(bot): connect new middleware check is registration

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -3,10 +3,11 @@ from aiogram.fsm.storage.memory import MemoryStorage
 import os
 from dotenv import load_dotenv
 
-from app.bot.middleware import DBMiddleware
+from app.bot.middleware import DBMiddleware, AuthMiddleware
 
 load_dotenv()
 
 bot = Bot(os.getenv("BOT_TOKEN"))
 dp = Dispatcher(storage=MemoryStorage())
 dp.update.middleware(DBMiddleware())
+dp.update.middleware(AuthMiddleware())
