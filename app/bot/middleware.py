188cf4243ec92651651c52b04aeda012509a614a
Игорь <idemenko22@gmail.com>
2026-04-29 15:03:14 +1000

    feat(bot): add middleware for check registration

--- app/bot/middleware.py
+++ app/bot/middleware.py
@@ -1,6 +1,6 @@
 from aiogram.fsm.middleware import BaseMiddleware
 from app.db.database import async_session
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, Update
 from app.services.user import check_register
 from app.bot.keyboard import register_kb
 
@@ -16,16 +16,29 @@ class DBMiddleware(BaseMiddleware):
 
 class AuthMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data):
-        user = data.get("event_from_user")
+        logger.info("ENTER IN MIDDLEWARE CHECK REGISTER")
+        user = getattr(event, "user", None)
+        logger.info("MIDDLEWARE - user=%s", user)
         if not user:
             return await handler(event, data)
+
         logger.info("AuthMiddleware user.id=%s", user.id)
         if not await check_register(user.id):
-            if isinstance(event, Message):
-                await event.answer("❌ Вы не зарегистрированы.\nНажмите кнопку ниже 👇", reply_markup=await register_kb())
-            elif isinstance(event, CallbackQuery):
-                await event.message.answer("❌ Вы не зарегистрированы.\nНажмите кнопку ниже 👇", reply_markup=await register_kb())
+            message = None
+            callback = None
+            if isinstance(event, Update):
+                message = event.message
+                callback = event.callback_query
+            logger.info("MIDDLEWARE - user dont register type_event=%s", type(event))
+            if message:
+                logger.info("MIDDLEWARE - type_event=Message event.text=%s", message.text)
+                if message.text.startswith("/start"):
+                    logger.info("MIDDLEWARE - user typing=/start")
+                    return await handler(event, data)
+                await message.answer("❌ Вы не зарегистрированы.\nНажмите кнопку ниже 👇", reply_markup=await register_kb())
+            elif callback:
+                await callback.message.answer("❌ Вы не зарегистрированы.\nНажмите кнопку ниже 👇", reply_markup=await register_kb())
+            logger.info("Middleware user not register - block handler")
             return
 
         return await handler(event, data)
-
