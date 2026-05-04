a841701c5f4c55a67c52b8160fa07d92eb35b551
Игорь <idemenko22@gmail.com>
2026-05-04 18:31:50 +1000

    feat(db): create record in category table on start bot

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,12 +1,24 @@
 from dotenv import load_dotenv
 import os
+import logging
 
 from app.db.database import Base, engine
+from app.db import crud
 
 load_dotenv()
+logger = logging.getLogger(__name__)
 
 async def init_models():
     async with engine.begin() as conn:
         if os.getenv("AUTO_CREATE_TABLES") != "1":
             return
         await conn.run_sync(Base.metadata.create_all)
+        logger.info("CREATED TABLES")
+        categories = await crud.get_categories_all()
+        has_transfer_category = False
+        transfer_category = str(os.getenv("CATEGORY_FOR_TRANSFER"))
+        for category in categories:
+            if category.name == transfer_category:
+                has_transfer_category = True
+        if not has_transfer_category:
+            await crud.create_category(transfer_category)
