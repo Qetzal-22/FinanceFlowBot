77a0dd999901f06eda325f1dd798ca49235d7d10
Игорь <idemenko22@gmail.com>
2026-05-07 18:57:30 +1000

    refactor(api): change older start api on modern lifespan

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 import asyncio
 
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from uvicorn import Config, Server
 from starlette.middleware.sessions import SessionMiddleware
@@ -10,13 +11,22 @@ from starlette.middleware.sessions import SessionMiddleware
 from app.api.routers.admin import admin_router
 from app.config.logging_config import setup_logging
 from app.core.startup import startup
+from app.core.shutdown import shutdown
 
 setup_logging()
 
 logger = logging.getLogger(__name__)
 load_dotenv()
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await startup()
+    yield
+    await shutdown()
+
+
+app = FastAPI(title="Finance Flow", lifespan=lifespan)
 
 app.include_router(admin_router)
 
@@ -24,7 +34,3 @@ app.add_middleware(
     SessionMiddleware,
     secret_key=os.getenv("SESSION_SECRET_KEY")
 )
-
-@app.on_event("startup")
-async def on_startup():
-    await startup()
